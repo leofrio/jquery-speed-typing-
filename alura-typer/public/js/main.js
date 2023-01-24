cda682e937d46b512cb681bc35ce0cf39e412f85
leofrio <951549927@adm.unifor.br@DTEC65046812.unifor.br>
2023-01-24 15:20:47 -0300

    fixing restart bug

--- alura-typer/public/js/main.js
+++ alura-typer/public/js/main.js
@@ -1,7 +1,8 @@
 let phrase= $(".phrase").text();
+let trimmedPhrase=phrase.trim()
 let wordCount=phrase.trim().split(" ").length;
 let timeLimit=$("#time-limit");
-timeLimit.text(wordCount);
+timeLimit.text(wordCount+2);
 let phraseSize=$("#phrase-size");
 phraseSize.text(wordCount);
 let gameButton=$(".game-button")
@@ -10,28 +11,36 @@ let typingArea=$(".typing-area");
 typingArea.on("input",function () {
     $("#word-counter").text(typingArea.val().trim().length !== 0 ? typingArea.val().trim().split(" ").length : 0);
     $("#char-counter").text(typingArea.val().trim().length);
+
 })
 let timelimitValue=parseInt(timeLimit.text());
-gameButton.on("click",game)
+gameButton.on("click",function() {
+    game();
+})
 
 function game() {
-    timelimitValue=wordCount;
+    gameButton.attr("disabled",true)
+    timelimitValue=wordCount +2;
     timeLimit.text(timelimitValue);
     typingArea.attr("disabled",false)
     typingArea.focus();
     typingArea.val("")
     gameButton.html("Restart");
+    gameButton.removeClass("start")
+    gameButton.addClass("restart")
     console.log('test');
     let chronoId=setInterval(function() {
+        gameButton.attr("disabled",false)
         timelimitValue > 0 ? timelimitValue-- : 0;
         timeLimit.text(timelimitValue);
         if(timelimitValue < 1) {
             typingArea.attr("disabled",true)
             clearInterval(chronoId);
         }
-        gameButton.one("click",function(event) {
+        $(".restart").one("click",function(event) {
             clearInterval(chronoId)
-            console.log("i get here")
+            $("#word-counter").text(0);
+            $("#char-counter").text(0);
         })
     }, 1000)
 }
