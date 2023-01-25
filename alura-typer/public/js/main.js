ebccc8ecfc09cc20c3d3e7431c3f71f487b78ea0
leofrio <951549927@adm.unifor.br@DTEC65046812.unifor.br>
2023-01-25 16:05:24 -0300

    stylizing and highlighting

--- alura-typer/public/js/main.js
+++ alura-typer/public/js/main.js
@@ -6,7 +6,9 @@ timeLimit.text(wordCount+2);
 let phraseSize=$("#phrase-size");
 phraseSize.text(wordCount);
 let gameButton=$(".game-button")
-gameButton.html("Start")
+let buttonText=$(".button-text")
+let gameButtonIcon=$("#game-button-icon")
+buttonText.text("Start")
 let typingArea=$(".typing-area");
 let timelimitValue=parseInt(timeLimit.text());
 let resultMessaage=$("#result-message")
@@ -24,10 +26,36 @@ function game() {
 function eachInput(chronoId){
     typingArea.on("input",function () {
         updateCount()
+        highlightRightWrong()
         if(checkIfWon()) {
             endGame(chronoId,1)
         }
     })
+}
+function highlightRightWrong() {
+    let typed=typingArea.val();
+    let trimmedPhrase=phrase.trim()
+    let comparable=trimmedPhrase.substring(0,typed.length)
+    let incomparable=typed.length >= trimmedPhrase.length ? "" : trimmedPhrase.substring(typed.length,trimmedPhrase.length)
+    let right =checkWhatWasRightBefore(comparable.length)
+    let wrong=comparable.replace(right,"")
+    $(".phrase").html("" +
+        "<span class='right-highlight'>" + right + "</span>" +
+        "<span class='wrong-highlight'>" + wrong + "</span>" +
+        incomparable)
+    console.log(right )
+}
+function checkWhatWasRightBefore(lastIndex) {
+    if(lastIndex === 0) {
+        return ""
+    }
+    let trimmedSubPhrase=phrase.trim().substring(0,lastIndex)
+    let typedSub=typingArea.val().substring(0,lastIndex);
+    if(typedSub === trimmedSubPhrase) {
+        return typedSub
+    }else {
+        return checkWhatWasRightBefore(lastIndex-1)
+    }
 }
 function updateCount() {
     $("#word-counter").text(typingArea.val().trim().length !== 0 ? typingArea.val().replace(/\s+/g, ' ').trim().split(" ").length : 0);
@@ -37,12 +65,14 @@ function startGame() {
     gameButton.attr("disabled",true)
     timelimitValue=wordCount +2;
     timeLimit.text(timelimitValue);
+    $(".phrase").text("one two three four five")
     typingArea.attr("disabled",false)
     typingArea.focus();
     typingArea.val("")
     resultMessaage.text("")
-    gameButton.html("Restart");
+    buttonText.text("Restart")
     gameButton.removeClass("start")
+    gameButtonIcon.text("restore")
     typingArea.removeClass("disabled-area")
     gameButton.addClass("restart")
 }
